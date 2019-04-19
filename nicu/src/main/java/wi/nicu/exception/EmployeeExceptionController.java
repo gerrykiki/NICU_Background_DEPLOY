b6b618d1b2f5b5124fb9efaf5f293f7b09b07cb6
Berlin <berlin_lin@wistron.com>
2019-04-19 13:55:45 +0800

    no message

--- nicu/src/main/java/wi/nicu/exception/EmployeeExceptionController.java
+++ nicu/src/main/java/wi/nicu/exception/EmployeeExceptionController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class EmployeeExceptionController {
 	@ExceptionHandler(value = EmployeeNotFoundException.class) //Used to handle the specific exceptions and sending the custom responses to the client
 	public ResponseEntity<Object> exception(EmployeeNotFoundException exception){
-		return new ResponseEntity<>("null",HttpStatus.NOT_FOUND);
+		return new ResponseEntity<>("null",HttpStatus.NOT_FOUND); //Return Http body and status code
 	}
 }
